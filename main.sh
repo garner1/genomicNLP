fdb13ba416f1919fa5799ce891945c6a4a49721c
garner1 <silvano.garnerone@gmail.com>
2018-03-19 15:44:40 +0100

    minor

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# bash process_reference.sh /home/garner1/Work/dataset/genomicNLP AAGCTT /home/garner1/Work/genomes/hg19.fa
+# bash module/process_reference.sh /home/garner1/Work/dataset/genomicNLP AAGCTT /home/garner1/Work/genomes/hg19.fa
 
 # barcode=ACGACATC
 # exp=M1A-1_XZ31_"$barcode"
@@ -9,7 +9,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=TGATGATC
 # exp=M1A-2_XZ31_TGATGATC
@@ -18,7 +18,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=CATCATCC
 # exp=M1A-3_XZ31_CATCATCC
@@ -27,7 +27,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=GTCGTTCC
 # exp=M1A-4_XZ31_GTCGTTCC
@@ -36,7 +36,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=ACGACTCC
 # exp=M1A-5_XZ31_ACGACTCC
@@ -45,7 +45,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=TGATGGAA
 # exp=M1B-5_XZ31_TGATGGAA
@@ -54,7 +54,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=TCACACGC
 # exp=M1B-6_XZ31_TCACACGC
@@ -63,7 +63,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=GTCGTCGC
 # exp=T1-2_XZ31_GTCGTCGC
@@ -72,7 +72,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=ACGACCGC
 # exp=T1-3_XZ31_ACGACCGC
@@ -81,7 +81,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 # barcode=CATCAATC
 # exp=T1-5_XZ31_CATCAATC
@@ -90,7 +90,7 @@
 # ref=/home/garner1/Work/genomes/hg19.fa
 # bamfile=~/Work/dataset/CNV/bamfiles/patient_1/selection/"$exp".bam
 # source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-# bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+# bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 ###########################################
 barcode=TCACAGAG
 exp=M2A-1_XZ31_TCACAGAG
@@ -99,7 +99,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=GACACGAG
 exp=M2A-3_XZ31_GACACGAG
@@ -108,7 +108,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=TAGGTCGC
 exp=M2B-1_XZ31_TAGGTCGC
@@ -117,7 +117,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=CTAACTCA
 exp=M2B-2_XZ31_CTAACTCA
@@ -126,7 +126,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=GCTTGTCA
 exp=M2B-3_XZ31_GCTTGTCA
@@ -135,7 +135,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=ATGTGCGC
 exp=T2-2_XZ31_ATGTGCGC
@@ -144,7 +144,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=TCACATCA
 exp=T2-3_XZ31_TCACATCA
@@ -153,7 +153,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=CGTGTTCA
 exp=T2-4_XZ31_CGTGTTCA
@@ -162,7 +162,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=GACACTCA
 exp=T2-5_XZ31_GACACTCA
@@ -171,7 +171,7 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 barcode=ATGTGTCA
 exp=T2-6_XZ31_ATGTGTCA
@@ -180,6 +180,6 @@ datadir_ref=/home/garner1/Work/dataset/genomicNLP
 ref=/home/garner1/Work/genomes/hg19.fa
 bamfile=~/Work/dataset/CNV/bamfiles/patient_2/selection/"$exp".bam
 source_bamfile=/media/bicroserver_2-seq/RESTSEQ_PIPELINE_OUTPUT/XZ31/outdata/"$barcode".sorted.bam
-bash process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
+bash module/process_experiment.sh $datadir_exp $datadir_ref $ref $bamfile $source_bamfile
 
 
